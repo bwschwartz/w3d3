d2f1925fb3ac892ba21afc2379121e139c548185
Benjamin Schwartz <bs4308@nyu.edu>
2022-08-03 18:04:41 -0400

    Final

--- recursion.rb
+++ recursion.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 def range_recur(start, last)
   return [] if last <= start
   [start] + range_recur(start + 1, last)
@@ -55,17 +56,92 @@ def fib(n)
 end
 
 def bsearch(arr,target)
-  arr.length.even? ? i = arr.length/2 - 1 : i = arr.length/2 
+  arr.length.even? ? i = arr.length/2 - 1 : i = arr.length/2
   return i if arr[i] == target
   return nil if !arr.include?(target)
   left, right = arr[0...i], arr[i+1..-1]
-  bsearch(left,target) || bsearch(right,target) + i + 1 
+  bsearch(left,target) || bsearch(right,target) + i + 1
 end
 
-p bsearch([1, 2, 3], 1) # => 0
-p bsearch([2, 3, 4, 5], 3) # => 1
-p bsearch([2, 4, 6, 8, 10], 6) # => 2
-p bsearch([1, 3, 4, 5, 9], 5) # => 3
-p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
-p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
-p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+
+def merge(arr1, arr2)
+  merged = []
+  while arr1.length >0  && arr2.length > 0
+    if arr1[0] < arr2[0]
+      merged << arr1[0]
+      arr1.delete_at(0)
+    else
+      merged << arr2[0]
+      arr2.delete_at(0)
+    end
+  end
+ merged + arr1 + arr2
+end
+
+arr1 = [3, 27, 100]
+arr2 = [9, 10, 1000, 100000]
+
+# p merge(arr1, arr2)
+
+def merge_sort(arr)
+  return arr if arr.length <= 1
+  mid = arr.length / 2
+  left, right = arr[0...mid], arr[mid..-1]
+  merge(merge_sort(left), merge_sort(right))
+end
+
+# p merge_sort([38, 27, 43, 3, 9, 82, 10, 100, 14, -12])
+
+
+def subsets(arr)
+  return [arr] if arr.length == 0
+  last = arr[-1]
+  without_last = subsets(arr[0...-1])
+  without_last + without_last.map { |sub| sub + [last]}
+end
+# p (subsets([1, 2]))
+# p subsets([1, 2, 3])
+# [[], [1], [2], [1, 2]]
+
+def perm(arr)
+  return arr if arr.length == 1
+  return [[arr.first, arr.last], [arr.last, arr.first]] if arr.length == 2
+
+  last = arr[-1]
+  smaller_perm = perm(arr[0...-1])
+
+  perms = []
+
+  smaller_perm.each do |permutation|
+    temp_perm = permutation.deep_dup
+    for i in (0..permutation.length)
+      perms << temp_perm.deep_dup.insert(i, last)
+    end
+  end
+  perms
+end
+
+p perm([1, 2, 3, 4, 5])
+
+def greedy_make_change(amt, coins)
+  # coins = [25, 10, 5, 1]
+  # amt = 0 < coins[-1]
+  return nil if amt < coins[-1]
+  out_coins = []
+  while amt > 0
+    if amt - coins[0] > 0
+      amt -= coins[0]
+      out_coins = coins[0]
+    end
+  end
+  out_coins += greedy_make_change(amt, coins[1..-1])
+end
+
+p greedy_make_change(32, [25, 10, 5, 1])
